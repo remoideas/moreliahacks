2bc401e4f9944ea8fdbf0c5cb0e40fd31d514ceb
ZeroTrace <julio.ortega.contreras@gmail.com>
2015-12-12 17:30:43 -0600

    Colaboradores e iconos

---
+++ moreliahacks/resources/views/colaboradores.blade.php
@@ -0,0 +1,193 @@
+    @extends('template.wrapp')
+
+    @section('content')
+
+    <!-- START MAIN -->
+    <div id="main">
+        <!-- START WRAPPER -->
+        <div class="wrapper">
+
+            <!-- START CONTENT -->
+            <section id="content">
+
+                <!--start container-->
+                <div class="container">
+
+                <div id="card-stats" class="seaction">
+                <h4 class="header">APIs de Facebook y Twitter</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  blue white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>           
+                <h4 class="header">Crawling</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  teal white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Datos abiertos</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content green white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Geo localizacion</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  light-green white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Procesamiento de lenguaje natural</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  lime white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Hugo Chanocua Herrejon </p>
+                                        <p class="card-stats-compare">  hugo.chanocua@gmail.com </p>
+                                        <p class="card-stats-compare">  https://mx.linkedin.com/in/hugochanocua </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Visualizacion de datos</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  amber white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Juan Adrian Garcia Cid</p>
+                                        <p class="card-stats-compare"><img src="{!! asset('layout03/images/gmail.png') !!}" alt="correo"><span class="logo-text"> juanadrian@iCloud.com</span></p>
+                                        <p class="card-stats-compare"><a href="https://www.fb.com/JuanAdrianMx" class="brand-logo darken-1"><img src="{!! asset('layout03/images/facebook.png') !!}" alt="facebook"></a><span class="logo-text"> /JuanAdrianMx</span></p>
+                                        <p class="card-stats-compare"><a href="https://mx.linkedin.com/in/juanadrian" class="brand-logo darken-1"><img src="{!! asset('layout03/images/linkedin.png') !!}" alt="linkedin"></a><span class="logo-text"> /juanadrian</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  amber white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Juan Adrian Garcia Cid</p>
+                                        <p class="card-stats-compare"><img src="{!! asset('layout03/images/gmail.png') !!}" alt="correo"><span class="logo-text"> juanadrian@iCloud.com</span></p>
+                                        <p class="card-stats-compare"><a href="https://www.fb.com/JuanAdrianMx" class="brand-logo darken-1"><img src="{!! asset('layout03/images/facebook.png') !!}" alt="facebook"></a><span class="logo-text"> /JuanAdrianMx</span></p>
+                                        <p class="card-stats-compare"><a href="https://mx.linkedin.com/in/juanadrian" class="brand-logo darken-1"><img src="{!! asset('layout03/images/linkedin.png') !!}" alt="linkedin"></a><span class="logo-text"> /juanadrian</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  amber white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Juan Adrian Garcia Cid</p>
+                                        <p class="card-stats-compare"><img src="{!! asset('layout03/images/gmail.png') !!}" alt="correo"><span class="logo-text"> juanadrian@iCloud.com</span></p>
+                                        <p class="card-stats-compare"><a href="https://www.fb.com/JuanAdrianMx" class="brand-logo darken-1"><img src="{!! asset('layout03/images/facebook.png') !!}" alt="facebook"></a><span class="logo-text"> /JuanAdrianMx</span></p>
+                                        <p class="card-stats-compare"><a href="https://mx.linkedin.com/in/juanadrian" class="brand-logo darken-1"><img src="{!! asset('layout03/images/linkedin.png') !!}" alt="linkedin"></a><span class="logo-text"> /juanadrian</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  amber white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Juan Adrian Garcia Cid</p>
+                                        <p class="card-stats-compare"><img src="{!! asset('layout03/images/gmail.png') !!}" alt="correo"><span class="logo-text"> juanadrian@iCloud.com</span></p>
+                                        <p class="card-stats-compare"><a href="https://www.fb.com/JuanAdrianMx" class="brand-logo darken-1"><img src="{!! asset('layout03/images/facebook.png') !!}" alt="facebook"></a><span class="logo-text"> /JuanAdrianMx</span></p>
+                                        <p class="card-stats-compare"><a href="https://mx.linkedin.com/in/juanadrian" class="brand-logo darken-1"><img src="{!! asset('layout03/images/linkedin.png') !!}" alt="linkedin"></a><span class="logo-text"> /juanadrian</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Resiliencia</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  orange white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Movilidad</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content  red white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                </div>
+                <h4 class="header">Prevencion del delito</h4>
+                <div class="row">
+                            <div class="col s12 m6 l3">
+                                <div class="card">
+                                    <div class="card-content brown white-text">
+                                        <p class="card-stats-title"><i class="mdi-action-perm-identity"></i> Nombre </p>
+                                        <p class="card-stats-compare"> Email </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                </div>
+                <!--end container-->
+            </section>
+            <!-- END CONTENT -->
+
+            <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+        </div>
+        <!-- END WRAPPER -->
+
+    </div>
+    <!-- END MAIN -->
+
+
+    @endsection
+    @section ('scripts')
+        <!-- google map api -->
+    <script type="text/javascript" src="#"></script>
+
+    <!--jvectormap-->
+    <script type="text/javascript" src="{!! asset('layout03/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('layout03/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('layout03/js/plugins/jvectormap/vectormap-script.js') !!}"></script>
+
+
+    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="{!! asset('layout03/js/plugins.js') !!}"></script>
+    <!-- Toast Notification -->
+    <script type="text/javascript">
+    // Toast Notification
+    $(window).load(function() {
+        setTimeout(function() {
+            Materialize.toast('<span>Hiya! I am a toast.</span>', 1500);
+        }, 1500);
+        setTimeout(function() {
+            Materialize.toast('<span>You can swipe me too!</span>', 3000);
+        }, 5000);
+        setTimeout(function() {
+            Materialize.toast('<span>You have new order.</span><a class="btn-flat yellow-text" href="#">Read<a>', 3000);
+        }, 15000);
+    });
+
+    </script>
+    @endsection
