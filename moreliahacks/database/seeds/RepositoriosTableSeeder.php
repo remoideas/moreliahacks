9fa7bde363e51202b4c0d2ec855b3a11ca9f38cb
Antonio Reyes <antremo@gmail.com>
2015-12-12 18:19:27 -0600

    Seeder

--- moreliahacks/database/seeds/RepositoriosTableSeeder.php
+++ moreliahacks/database/seeds/RepositoriosTableSeeder.php
@@ -11,6 +11,47 @@ class RepositoriosTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('repositorios')->insert([
+            'nombre' => str_random(10),
+            'descripcion' => str_random(10).'@gmail.com',
+            'autores' => str_random(10),
+            'fecha_publicacion' => '20015-12-12',
+            'link' => str_random(10),
+            'etiquetas' => str_random(10),
+            'inputs' => str_random(10),
+            'outputs' => str_random(10),
+            'lenguaje_backend' => str_random(10),
+            'lenguaje_frontend' => str_random(10),
+        ]);
+
+        DB::table('repositorios')->insert([
+            'nombre' => str_random(10),
+            'descripcion' => str_random(10).'@gmail.com',
+            'autores' => str_random(10),
+            'fecha_publicacion' => '20015-12-12',
+            'link' => str_random(10),
+            'etiquetas' => str_random(10),
+            'inputs' => str_random(10),
+            'outputs' => str_random(10),
+            'lenguaje_backend' => str_random(10),
+            'lenguaje_frontend' => str_random(10),
+        ]);
+
+
+        DB::table('repositorios')->insert([
+            'nombre' => str_random(10),
+            'descripcion' => str_random(10).'@gmail.com',
+            'autores' => str_random(10),
+            'fecha_publicacion' => '20015-12-12',
+            'link' => str_random(10),
+            'etiquetas' => str_random(10),
+            'inputs' => str_random(10),
+            'outputs' => str_random(10),
+            'lenguaje_backend' => str_random(10),
+            'lenguaje_frontend' => str_random(10),
+        ]);
+
+
     }
 }
